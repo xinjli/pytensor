5c370c34d71b2c973efa9e6d0d3c13928b597939
xinjli <rigel@xinjli.com>
2016-11-14 00:32:48 +0900

    Add simple nn

--- network/common/activation.py
+++ network/common/activation.py
@@ -7,6 +7,13 @@ def step_function(x):
     else:
         return 0
 
+def softmax(x):
+    c = np.max(x)
+    exp_x = np.exp(x - c)
+    sum_exp_x = np.sum(exp_x)
+    y = exp_x / sum_exp_x
+
+    return y
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
