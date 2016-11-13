e4b8cdde8b901c32333507e3bb2bae6cbb593c28
xinjli <rigel@xinjli.com>
2016-11-14 00:05:05 +0900

    Add gradient descent

--- network/common/gradient.py
+++ network/common/gradient.py
@@ -21,3 +21,12 @@ def numerical_gradient(f, x):
         x[idx] = tmp_val
 
     return grad
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
