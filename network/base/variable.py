88132ca8cd6dd1c683f8162fd038ae01e00d5bb3
lixinjian <lixinjian@lixinjians-MBP.hsd1.pa.comcast.net>
2017-09-18 09:46:37 -0400

    update structure

---
+++ network/base/variable.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Variable:
+
+    def __init__(self, value, name='Variable', trainable=True):
+        """
+        :param value: numpy val
+        :param index:
+        """
+        self.value = np.array(value)
+        self.grad = np.zeros(self.value.shape)
+
+        self.name = name
+
+        self.trainable = trainable
+
+    def clear_grad(self):
+        self.grad = np.zeros(self.value.shape)
