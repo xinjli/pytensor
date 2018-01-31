641ce7d855b8f9c01da57ba81bee33f05dae3799
xinjli <lixinjian1217@gmail.com>
2018-01-30 21:00:37 -0500

    upload setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
    name='pytensor',
-   version='0.1',
+   version='0.101',
    description='A deep learning framework using pure numpy',
    author='Xinjian Li',
    author_email='lixinjian1217@gmail.com',
-   packages=['pytensor'],  #same as name
+   packages=find_packages(),
    install_requires=['scikit-learn', 'numpy'], #external packages as dependencies
 )
